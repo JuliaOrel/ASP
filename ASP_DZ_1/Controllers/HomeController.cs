31f15ec37b7b5c2a0571864eaa3880a1fbd14556
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-02-03 22:34:26 +0200

    Добавьте файлы проекта.

---
+++ ASP_DZ_1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_DZ_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var rand = new Random();
+            char letter = Convert.ToChar(rand.Next(65, 90));
+            ViewData["someKey"] = letter;
+            return View();
+        }
+        public IActionResult Restaurants()
+        {
+            return View();
+        }
+
+        public IActionResult Countries()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
