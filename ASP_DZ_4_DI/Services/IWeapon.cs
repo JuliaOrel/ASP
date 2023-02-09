6e7c11dc2cdc02f83b939c36726dd0e5aa261f39
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-02-09 19:30:27 +0200

    DI

---
+++ ASP_DZ_4_DI/Services/IWeapon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_DZ_4_DI.Services
+{
+    public interface IWeapon
+    {
+    }
+}
