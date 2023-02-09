9650ceaa9a5559c11d15b3f8cb0739bc4436bd58
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-02-09 17:25:38 +0200

    Services

---
+++ Meeting2_Services/Services/EmailMessageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meeting2_Services.Services
+{
+    public class EmailMessageService
+    {
+    }
+}
---
+++ Meeting2_Services/Services/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meeting2_Services.Services
+{
+    public interface IMessageService
+    {
+        string SendMessage();
+    }
+}
