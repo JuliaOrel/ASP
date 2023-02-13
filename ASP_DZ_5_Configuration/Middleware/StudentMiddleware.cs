edef72778cbd3a595c6a04d1c3302e15d860bd6e
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-02-13 20:58:37 +0200

    json

--- ASP_DZ_5_Configuration/Middleware/StudentMiddleware.cs
+++ ASP_DZ_5_Configuration/Middleware/StudentMiddleware.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ASP_DZ_5_Configuration.Middleware
@@ -21,16 +22,24 @@ namespace ASP_DZ_5_Configuration.Middleware
         }
         public async Task InvokeAsync(HttpContext context)
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.Append($"<p>Name: {Student?.Name}</p>");
-            stringBuilder.Append($"<p>Lastname: {Student?.Lastname}</p>");
-            stringBuilder.Append($"<p>Age: {Student?.Age}</p>");
-            stringBuilder.Append("<h3>Disciplines</h3><ul>");
-            foreach (string disc in Student.Disciplines)
-                stringBuilder.Append($"<li>{disc}</li>");
-            stringBuilder.Append("</ul>");
+            //StringBuilder stringBuilder = new StringBuilder();
+            //stringBuilder.Append($"<p>Name: {Student?.Name}</p>");
+            //stringBuilder.Append($"<p>Lastname: {Student?.Lastname}</p>");
+            //stringBuilder.Append($"<p>Age: {Student?.Age}</p>");
+            //stringBuilder.Append("<h3>Disciplines</h3><ul>");
+            //foreach (string disc in Student.Disciplines)
+            //    stringBuilder.Append($"<li>{disc}</li>");
+            //stringBuilder.Append("</ul>");
 
-            await context.Response.WriteAsync(stringBuilder.ToString());
+            //or
+            var jsonOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            string json = JsonSerializer.Serialize(Student, jsonOptions);
+
+            //await context.Response.WriteAsync(stringBuilder.ToString());
+            await context.Response.WriteAsync(json);
         }
     }
 }
