6f632c389ae0cb147f82885a68490827543f8b21
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-02-14 20:41:19 +0200

    Books

---
+++ ASP_DZ_6_Books/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_DZ_6_Books.Models
+{
+    public class Book
+    {
+        public string NameBook { get; set; }
+        public string FIOAuthor { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public int YearIssue { get; set; }
+    }
+}
