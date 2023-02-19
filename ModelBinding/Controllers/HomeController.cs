f682e1377954e8459c7f3fc1aeb899c2e755c2b0
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-02-19 17:24:27 +0200

    Trail binding

---
+++ ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        static List<Trail> trails = new List<Trail>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(trails);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTrail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Trail trail)
+        {
+            //trail.Id = Guid.NewGuid().ToString();
+            trails.Add(trail);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelBinding/Models/Trail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinding.Models
+{
+    public class Trail
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public float TrailLength { get; set; }
+    }
+}
