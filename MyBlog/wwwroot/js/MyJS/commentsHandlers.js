414aa607c4df9cc034823f85b0de4bf6db4cc3f9
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-02-24 22:59:33 +0200

    Js+commentsController

---
+++ MyBlog/wwwroot/js/MyJS/commentsHandlers.js
@@ -0,0 +1,37 @@
+﻿async function mainCommentHandler(event) {
+
+    event.preventDefault();
+    let form = document.forms.mainCommentForm;
+    let message = form.elements.Message.value;
+    let postId = form.elements.postId.value;
+
+    const response = await fetch("/Comments/CreateComment", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            message: message,
+            postId: postId
+        })
+    });
+
+    if (response) {
+
+        recountComments(1);
+        const writeParentCommentAndChildrenPartial = await response.text();
+        document.getElementById("mainCommentId").innerHTML += writeParentCommentAndChildrenPartial;
+    }
+    else {
+        console.log(response.statusText);
+    }
+}
+
+function recountComments(count) {
+
+    let commentsCountElement = document.getElementById("commentsCountId");
+    let commentsCount = commentsCountElement.innerText;
+    commentsCount = +commentsCount + count;
+    commentsCountElement.innerText = commentsCount;
+}
