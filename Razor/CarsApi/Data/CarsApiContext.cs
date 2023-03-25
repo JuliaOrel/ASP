b2a8917e6caef33b0bce225b6369a45dc6cfa95f
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-03-25 21:19:16 +0200

    cars

---
+++ Razor/CarsApi/Data/CarsApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace CarsApi.Data
+{
+    public class CarsApiContext : DbContext
+    {
+        public CarsApiContext (DbContextOptions<CarsApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shared.Models.Car> Car { get; set; }
+    }
+}
---
+++ Razor/Shared/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int YearIssue { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+    }
+}
