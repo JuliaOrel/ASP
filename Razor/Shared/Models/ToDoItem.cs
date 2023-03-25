e0ae08c454933e2f707b50ba883a53d7a825874d
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-03-25 16:34:27 +0200

    API

---
+++ Razor/Shared/Models/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Action { get; set; }
+        public string Tip { get; set; }
+        public int TimeToComplete { get; set; }
+        public bool IsCompleted { get; set; }
+        public string SecretTip { get; set; }
+    }
+}
---
+++ Razor/WebApiQuickOverView/Data/WebApiQuickOverViewContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace WebApiQuickOverView.Data
+{
+    public class WebApiQuickOverViewContext : DbContext
+    {
+        public WebApiQuickOverViewContext (DbContextOptions<WebApiQuickOverViewContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shared.Models.ToDoItem> ToDoItem { get; set; }
+    }
+}
