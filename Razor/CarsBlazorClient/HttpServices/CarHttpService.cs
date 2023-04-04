d9acba05be51ee357f28b3e442d4adb525c77de3
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-04-04 19:21:20 +0300

    Blazor

--- Razor/CarsBlazorClient/HttpServices/CarHttpService.cs
+++ Razor/CarsBlazorClient/HttpServices/CarHttpService.cs
@@ -2,12 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CarsBlazorClient.HttpServices
 {
     public class CarHttpService : ICarHttpService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+        public CarHttpService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+        }
         public Task<CarDTO> DelCar(int id)
         {
             throw new NotImplementedException();
@@ -23,9 +36,24 @@ namespace CarsBlazorClient.HttpServices
             throw new NotImplementedException();
         }
 
-        public Task<List<CarDTO>> GetCars()
+        public async Task<List<CarDTO>> GetCars()
         {
-            throw new NotImplementedException();
+            HttpClient httpClient = CreateNamedHttpClient();
+            HttpResponseMessage response = await httpClient
+                .GetAsync("/GetCars");
+            
+            string content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode == false)
+            {
+                throw new ApplicationException(content);
+            }
+            var cars = JsonSerializer.Deserialize<List<CarDTO >> (content, _jsonSerializerOptions);
+            return cars;
+        }
+
+        private HttpClient CreateNamedHttpClient()
+        {
+            return _httpClientFactory.CreateClient("CarsHttpClient");
         }
 
         public Task<List<CarDetailsDTO>> GetCarsDetails()
--- Razor/CarsBlazorClient/Program.cs
+++ Razor/CarsBlazorClient/Program.cs
@@ -1,3 +1,4 @@
+using CarsBlazorClient.HttpServices;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,10 +21,14 @@ namespace CarsBlazorClient
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddHttpClient("CarsHttpClient", configure =>
             {
-                configure.BaseAddress = new Uri("https://localhost:5001/api/Cars")
+                configure.BaseAddress = new Uri("https://localhost:5001/api/Cars");
             });
-            
 
+            builder.Services.AddHttpClient<ICompanyHttpService, CompanyHttpService>(configure =>
+            {
+                configure.BaseAddress= new Uri("https://localhost:5001/api/Companies");
+            });
+            builder.Services.AddScoped<ICarHttpService, CarHttpService>();
             await builder.Build().RunAsync();
         }
     }
