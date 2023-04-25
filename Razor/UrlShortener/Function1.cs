ccb1f31d391cd90a170672ea31ef1a42cc73fa78
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-04-25 22:17:32 +0300

    Function

--- Razor/UrlShortener/Function1.cs
+++ Razor/UrlShortener/Function1.cs
@@ -7,29 +7,117 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Data.Tables;
+using UrlShortener.Models;
+using System.Linq;
 
 namespace UrlShortener
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("Set")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Table("ShortUrls")] TableClient tableClient,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string address = req.Query["address"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            address = address ?? data?.name;
+            if(string.IsNullOrEmpty(address))
+            {
+                return new OkObjectResult("Parameter 'address' was not written\n");
+            }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            UrlKey urlKey = null;
+            var result = await tableClient.GetEntityIfExistsAsync<UrlKey>("1", "Key");
+            if(result.HasValue==false)
+            {
+                urlKey = new UrlKey
+                {
+                    Id = 1000,
+                    PartitionKey = "1",
+                    RowKey = "Key"
+                };
+                await tableClient.UpsertEntityAsync(urlKey);
+            }
+            else
+            {
+                urlKey = result.Value;
+            }
+            int index = urlKey.Id;
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string code = string.Empty;
+            while(index>0)
+            {
+                code += alphabet[index % alphabet.Length];
+                index /= alphabet.Length;
+            }
+            code = string.Join("", code.Reverse());
+            urlKey.Id++;
+            await tableClient.UpsertEntityAsync<UrlKey>(urlKey);
+            UriData uriData = new UriData
+            {
+                Id = code,
+                Url = address,
+                Count = 1,
+                PartitionKey=$"{code[0]}",
+                RowKey=code,
+            };
 
-            return new OkObjectResult(responseMessage);
+            await tableClient.UpsertEntityAsync<UriData>(uriData);
+            return new OkObjectResult("Original url == " + uriData.Url +"\n"+
+                "Shorted URL == "+ uriData.PartitionKey);
+
         }
+
+        [FunctionName("Redirect")]
+        public static async Task<IActionResult> Redirect(
+            [HttpTrigger(
+            AuthorizationLevel.Anonymous,
+            "get",
+            "post",
+            Route ="Redirect/{shortUrl}")] HttpRequest httpRequest,
+            string shortUrl,
+            [Table("ShortUrls")] TableClient tableClient,
+            [Queue("requestCount")] IAsyncCollector<string> queue,
+            ILogger logger)
+        {
+            if(string.IsNullOrWhiteSpace(shortUrl))
+            {
+                return new BadRequestResult();
+            }
+            shortUrl = shortUrl.ToUpper();
+            var result = await tableClient
+                .GetEntityIfExistsAsync<UriData>(shortUrl[0].ToString(), shortUrl);
+            string url = "https://www.google.com.ua/?hl=uk";
+            if(result.HasValue && result.Value is UriData data)
+            {
+                url = data.Url;
+                await queue.AddAsync(data.RowKey);
+            }
+
+            return new RedirectResult(url);
+        }
+
+        [FunctionName("QueueShortUrlHandler")]
+        public static async Task QueueShortUrlHandler(
+            [QueueTrigger("requestsCount")] string shortUrl,
+            [Table("ShortUrl")] TableClient tableClient,
+            ILogger logger)
+        {
+            var result = await tableClient
+                .GetEntityIfExistsAsync<UriData>(shortUrl[0].ToString(), shortUrl);
+            if (result.HasValue && result.Value is UriData data)
+            {
+                data.Count++;
+                await tableClient.UpsertEntityAsync<UriData>(data);
+
+            }
+            }
     }
 }
--- Razor/UrlShortener/Models/UriData.cs
+++ Razor/UrlShortener/Models/UriData.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UrlShortener.Models
 {
-    public class UriData
+    public class UriData: ITableEntity
     {
         public string Id { get; set; }
         public string Url { get; set; }
         public int Count { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
     }
 }
