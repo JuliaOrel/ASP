509b5f601467041e9bbdf3398957c9fb562edb13
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-04-25 19:51:04 +0300

    Function

---
+++ Razor/UrlShortener/Models/UriData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrlShortener.Models
+{
+    public class UriData
+    {
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Razor/UrlShortener/Models/UrlKey.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrlShortener.Models
+{
+    public class UrlKey: ITableEntity
+    {
+        public int Id { get; set; }
+        public string PartitionKey { get;set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
